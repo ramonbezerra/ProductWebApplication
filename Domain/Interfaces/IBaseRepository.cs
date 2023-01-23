97ac21df831ec1246f99dc99c809f01728f80d0d
ramonbezerra <ramon.bezerra@outlook.com>
2023-01-23 07:45:48 -0300

    Criação de interfaces de repositório e serviço

---
+++ Domain/Interfaces/IBaseRepository.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces
+{
+    public interface IBaseRepository<TEntity> where TEntity : BaseEntity
+    {
+        void Insert(TEntity entity);
+        IEnumerable<TEntity> GetAll();
+        TEntity GetById(int id);
+        void Update(TEntity entity);
+        void Delete(TEntity entity);
+    }
+}
